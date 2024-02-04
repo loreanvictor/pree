b03af265085911d97c0d0c07dffbfa5768f41db5
loreanvictor <ghanizadeh.eugene@gmail.com>
2024-02-04 16:00:35 +0000

    Deploying to gh-pages from @ loreanvictor/pree@128722e284d0b93bd8ffe8f22860c89fc7ef2503 🚀

--- components/change-history.js
+++ components/change-history.js
@@ -1,61 +1,64 @@
 import { define, onFirstRender } from 'https://esm.sh/minicomp'
+import { html, ref } from 'https://esm.sh/rehtm'
 
 
 define('change-history', () => {
-  if (window.BUILD_ENV_API) {
-    onFirstRender(node => {
-      const root = node.shadowRoot
+  const date = ref()
+  const name = ref()
+  const version = ref()
 
-      const load = async () => {
-        const conf = await (await fetch(`${window.BUILD_ENV_API.baseURL}conf`)).json()
-        let path = window.location.pathname
+  const load = async () => {
+    const conf = await (await fetch(`${window.BUILD_ENV_API.baseURL}conf`)).json()
+    let path = window.location.pathname
 
-        if (conf.base && conf.base.length > 0) {
-          path = path.slice(conf.base.length + 2)
-        }
+    if (conf.base && conf.base.length > 0) {
+      path = path.slice(conf.base.length + 2)
+    }
 
-        if (conf.root && conf.root.length > 0) {
-          path = conf.root + '/' + path
-        }
+    if (conf.root && conf.root.length > 0) {
+      path = conf.root + '/' + path
+    }
 
-        if (path.endsWith('/')) {
-          path += 'index.html'
-        } else if (!path.endsWith('.html')) {
-          path += '.html'
-        }
+    if (path.endsWith('/')) {
+      path += 'index.html'
+    } else if (!path.endsWith('.html')) {
+      path += '.html'
+    }
 
-        const commit = await (await fetch(`${window.BUILD_ENV_API.baseURL}git/commits/last/${path}`)).json()
-        const pkg = JSON.parse(
-          await (await fetch(`${window.BUILD_ENV_API.baseURL}git/show/${commit.hash}:package.json`)).text()
-        )
-        const date = new Date(commit.date)
-          .toLocaleDateString('en-US', {
-            day: '2-digit',
-            month: 'short',
-            year: 'numeric'
-          })
+    const commit = await (await fetch(`${window.BUILD_ENV_API.baseURL}git/commits/last/${path}`)).json()
+    const pkg = JSON.parse(
+      await (await fetch(`${window.BUILD_ENV_API.baseURL}git/show/${commit.hash}:package.json`)).text()
+    )
 
-        root.innerHTML = `
-          <style>
-            div {
-              text-align: right;
-              opacity:.35;
-            }
-          </style>
-          <div>
-            <small>
-              <i>
-              updated at ${date}
-              <br> for ${pkg.name} version ${pkg.version}
-              </i>
-            </small>
-          </div>
-        `
-      }
+    date.current.textContent = new Date(commit.date)
+      .toLocaleDateString('en-US', {
+        day: '2-digit',
+        month: 'short',
+        year: 'numeric'
+      })
 
-      load()
-    })
+    name.current.textContent = pkg.name
+    version.current.textContent = pkg.version
   }
 
-  return ''
+  onFirstRender(() => {
+    if (window.BUILD_ENV_API) {
+      load()
+    }
+  })
+
+  return html`
+    <style>
+      div {
+        text-align: right;
+        opacity:.35;
+        font-size: 0.8rem;
+        font-style: italic;
+      }
+    </style>
+    <div>
+      Updated at <span ref=${date}>??</span> <br />
+      for <span ref=${name}>??</span> version <span ref=${version}>??</span>
+    </div>
+  `
 })
