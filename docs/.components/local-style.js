5e7349f69265196f006945efef692ad67bfd1baf
Eugene Ghanizadeh Khoub <ghanizadeh.eugene@gmail.com>
2024-02-10 12:12:47 +0100

    better deploy doc

--- docs/.components/local-style.js
+++ docs/.components/local-style.js
@@ -7,9 +7,10 @@ define('local-style', () => {
     const style = document.createElement('style')
     const cls = 'local-style-' + Math.random().toString(36).slice(2)
 
-    style.textContent = `.${cls} { \n ${node.textContent} \n }`
+    style.textContent = `.${cls}[data-local-style=${cls}] { \n ${node.textContent} \n }`
     parent.prepend(style)
     parent.classList.add(cls)
+    parent.setAttribute('data-local-style', cls)
 
     node.remove()
   })
