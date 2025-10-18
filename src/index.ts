cd5567db8dbf5b08c666c7d37cb7a96f1eae4810
Eugene Ghanizadeh Khoub <ghanizadeh.eugene@gmail.com>
2025-10-18 20:58:32 +0200

    fix type export syntax

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,7 @@
-export { serve, ServeOptions, Loader } from './serve'
-export { build, BuildOptions } from './build'
-export { view, ViewOptions } from './view'
+export { serve } from './serve'
+export { build } from './build'
+export { view } from './view'
+
+export type { ServeOptions, Loader } from './serve'
+export type { BuildOptions } from './build'
+export type { ViewOptions } from './view'
